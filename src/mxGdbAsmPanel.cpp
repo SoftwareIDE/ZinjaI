dea094170540b33d137f5221948061372f2709ff
zaskar <zaskar_84@yahoo.com.ar>
2016-02-25 21:39:00 -0300

    implemented a cache to avoid disassembling same function twice

--- src/mxGdbAsmPanel.cpp
+++ src/mxGdbAsmPanel.cpp
@@ -28,30 +28,66 @@ mxGdbAsmPanel::mxGdbAsmPanel (wxWindow * parent) : wxPanel(parent) {
 void mxGdbAsmPanel::Update ( ) {
 	m_asm_step_mode->SetValue(debug->IsAsmStepModeOn());
 	if (!debug->CanTalkToGDB()) return;
-	m_code->Clear(); 
-	wxString ans = debug->SendCommand("disassemble /m");
-	int current_line = 0;
-	do {
-		wxString line = ans.BeforeFirst('\n');
-		ans = ans.AfterFirst('\n');
-		if (line.StartsWith("~")) {
-			line = mxUT::UnEscapeString(line.Mid(1));
-			if (line.Last()=='\n') line.RemoveLast();
-			if (line.IsEmpty()) continue;
-			if (line.StartsWith("=>")) { 
-				current_line = m_code->GetLineCount()-1;
-				line[0]=' '; line[1]=' '; 
-			}
-			bool inner = line.StartsWith("   0x");
-			m_code->AppendLine(line,!inner);
+	
+	wxString pc = debug->InspectExpression("$pc").BeforeFirst(' ');
+	AddressRange::addr_t addr = AddressRange::Parse(pc);
+	if (m_current_range.Contains(addr)) { // si la función ya esta siendo mostrada
+		if (m_marker_handle!=-1) 
+			m_code->MarkerDeleteHandle(m_marker_handle);
+		m_marker_handle = -1;
+	} else {
+		map<AddressRange,wxString>::iterator it = m_cache.find(AddressRange(addr));
+		if (it==m_cache.end()) { // si es la primera vez que se analiza esa función
+			wxString ans = debug->SendCommand("disassemble /m");
+			AddressRange range = ParseCode(ans);
+			it = m_cache.insert(pair<AddressRange,wxString>(range,ans)).first;
+		} else { // si ya estaba desensamblada
+			ParseCode(it->second);
 		}
-	} while(ans.size());
-	if (current_line) {
-		m_code->MarkerAdd(current_line,m_code->next_available_marker);
-		m_code->EnsureVisibleEnforcePolicy(current_line);
+	}
+	map<AddressRange::addr_t,int>::iterator it = m_addr_to_line.find(addr);
+	if (it!=m_addr_to_line.end()) {
+		m_marker_handle = m_code->MarkerAdd(it->second,m_code->next_available_marker);
+		m_code->EnsureVisibleEnforcePolicy(it->second);
 	}
 }
 
 void mxGdbAsmPanel::OnCheckStepMove (wxCommandEvent & evt) {
 	debug->SetStepMode(m_asm_step_mode->GetValue());
 }
+
+mxGdbAsmPanel::AddressRange mxGdbAsmPanel::ParseCode (wxString ans) {
+	m_code->Clear(); 
+	m_marker_handle = -1;
+	m_addr_to_line.clear();
+	AddressRange range;
+	do {
+		wxString line = ans.BeforeFirst('\n');
+		ans = ans.AfterFirst('\n');
+		if (line.StartsWith("~")) {
+			// arreglar el formato
+			line = mxUT::UnEscapeString(line.Mid(1));
+			if (line.Last()=='\n') line.RemoveLast();
+			// evitar lineas en blanco
+			if (line.IsEmpty()) continue;
+			// quitar el indicador de la instruccion actual
+			if (line.StartsWith("=>")) { line[0]=' '; line[1]=' '; }
+			// determinar si es linea de código máquina, 
+			bool inner = false;
+			int i1=0, l=line.Len();
+			while(i1<l&&line[i1]==' ')++i1;
+			if (i1+2<l && line[i1]=='0' && line[i1+1]=='x') {
+				// y extrar la dirección para definir el rango de direcciones de la función
+				int i2 = i1; inner = true;
+				while(i2<l&&line[i2]!=' ')++i2;
+				range.m_to = AddressRange::Parse(line.Mid(i1,i2-i1));
+				m_addr_to_line[range.m_to] = m_code->GetLineCount()-1;
+				if (range.m_from==-1) range.m_from = range.m_to;
+			}
+			// mostrar la linea
+			m_code->AppendLine(line,!inner);
+		}
+	} while(ans.size());
+	return range;
+}
+
--- src/mxGdbAsmPanel.h
+++ src/mxGdbAsmPanel.h
@@ -2,17 +2,43 @@
 #define MXGDBASMPANEL_H
 #include <wx/panel.h>
 #include "Inspection.h"
+#include "Cpp11.h"
+#include <map>
 
 class mxStyledOutput;
 class wxCheckBox;
 
 class mxGdbAsmPanel : public wxPanel, public myDIGlobalEventHandler {
+	struct AddressRange {
+		typedef long long addr_t;
+		addr_t m_from, m_to;
+		static addr_t Parse(const wxString &address) {
+			addr_t result = 0;
+			for (int l = address.Len(), i = 2; i<l; ++i) { // 2 por saltear el 0x
+				result *= 16; 
+				char c = (address[i])|32;
+				if (c>='0'&&c<='9') result += address[i]-'0';
+				else /*(c>='a'&&c<='f')*/ result += (address[i]-'a'+10);
+			}
+			return result;
+		}
+		AddressRange() { m_from = m_to = -1; }
+		AddressRange(addr_t adr) { m_from = m_to = adr; }
+		bool operator<(const AddressRange &o) const { return m_to<o.m_from; }
+		bool Contains(addr_t ad) const { return ad>=m_from && ad<=m_to; }
+	};
+	int m_marker_handle=-1;
 	mxStyledOutput *m_code;
 	wxCheckBox *m_asm_step_mode;
+	AddressRange m_current_range;
+	map<AddressRange,wxString> m_cache;
+	map<AddressRange::addr_t,int> m_addr_to_line;
+	AddressRange ParseCode(wxString ans); 
 public:
 	mxGdbAsmPanel(wxWindow *parent);
 	void Update();
 	void OnDebugPausePost() override { Update(); }
+	void OnDebugStart() override { Update(); }
 	void OnCheckStepMove(wxCommandEvent &evt);
 	DECLARE_EVENT_TABLE();
 };
